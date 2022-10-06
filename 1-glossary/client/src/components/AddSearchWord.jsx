a776109803878bfc53324977e4f68b48534fd675
nhasty <nyall.hasty@gmail.com>
2022-10-05 17:06:45 -0700

    refactor Add button

--- 1-glossary/client/src/components/AddSearchWord.jsx
+++ 1-glossary/client/src/components/AddSearchWord.jsx
@@ -1,22 +1,22 @@
 import React, {useState} from 'react';
 
 const AddSearchWord = function({addClickHandler, searchClickHandler}) {
-  const [inputWord, setInputWord] = useState('');
+  const [inputWord, setInputWord] = useState(null);
   const [inputDefinition, setInputDefinition] = useState('')
-  const [addClicked, setAddClicked] = useState(0);
+  if (inputWord === '') {
+    setInputDefinition('');
+    setInputWord(null);
+  }
   return (
     <div>
       <input id='AddSearch' placeholder="Search or Add" onChange={(event) => setInputWord(event.target.value)}></input>
       <button onClick={() => {
-        if (addClicked === 0 && inputWord !== '') {
-          setAddClicked(1);
-        } else if (addClicked === 1 && inputWord !== '') {
+        if (inputWord && inputWord.length) {
           addClickHandler(inputWord, inputDefinition);
           document.getElementById('AddSearch').value = '';
-          setInputWord('');
           document.getElementById('Definition').value = '';
           setInputDefinition('');
-          setAddClicked(0)
+          setInputWord('');
         }
       }}>
         Add
@@ -28,22 +28,24 @@ const AddSearchWord = function({addClickHandler, searchClickHandler}) {
       }}>
         Search
       </button>
-      <Definition addClicked={addClicked} setInputDefinition={setInputDefinition} />
+      <Definition setInputDefinition={setInputDefinition} inputWord={inputWord}/>
     </div>
-
-
-  )
+  );
 };
-const Definition = function({addClicked, setInputDefinition}) {
-  if (addClicked === 1) {
-    return (
-      <input id="Definition" placeholder='Add a defintion' onChange={(event) => setInputDefinition(event.target.value)}></input>
-    )
+const Definition = function({addClicked, setInputDefinition, inputWord}) {
+  if (inputWord) {
+    if (inputWord !== '') {
+      return (
+        <input id="Definition" placeholder='Add a defintion' onChange={(event) => setInputDefinition(event.target.value)}></input>
+      );
+    } else {
+      return <div></div>;
+    }
   } else {
     return (
       <div></div>
-    )
+    );
   }
-}
+};
 
 export default AddSearchWord;
